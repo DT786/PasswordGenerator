ead08db6f3f0aab4f9e15adeaad4daa1a74493db
DT786 <118858895+DT786@users.noreply.github.com>
2023-04-04 22:33:21 +0100

    Add files via upload

---
+++ Password Generator.py
@@ -0,0 +1,38 @@
+import random
+import string
+import pyperclip
+import colorama
+from colorama import Fore, init
+colorama.init(convert = True)
+
+
+def pass_gen(length):
+    password = ''
+    chars = string.ascii_letters + string.digits + string.punctuation
+
+    print("How many passwords do you want to generate?")
+    numofpasswords = int(input())
+    copy = ''
+
+    if (numofpasswords == 1):
+        print(Fore.LIGHTBLUE_EX + "Do you want to copy the password to your clipboard?")
+        copy = input()
+
+    for i in range(numofpasswords):
+        for i in range(length):
+            password += random.choice(chars)
+            if (len(str(password)) == length):
+                print(password)
+                if (copy.lower() == "yes"):
+                    pyperclip.copy(password)
+                password = ''
+
+
+    return password
+
+print(Fore.LIGHTBLUE_EX + "How long do you want the password to be?")
+length2 = int(input())
+
+print(pass_gen(length2))
+
+input()
