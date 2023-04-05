f62894e8ead461b518a3b57a9a7213adf08048c6
DT786 <118858895+DT786@users.noreply.github.com>
2023-04-05 13:50:38 +0100

    Add files via upload

--- Password Generator.py
+++ Password Generator.py
@@ -24,7 +24,7 @@ def pass_gen(length):
         print(Fore.LIGHTBLUE_EX + "Do you want to copy the password to your clipboard? (y/n)")
         copy = input()
 
-        while (not copy.isalpha()): # This checks if the input contains only alphabeticals characters and not numbers.
+        while (not copy.isalpha()): # This checks if the input only contains alphabeticals characters.
             print(Fore.LIGHTBLUE_EX + "Do you want to copy the password to your clipboard? (y/n)")
             copy = input()
 
